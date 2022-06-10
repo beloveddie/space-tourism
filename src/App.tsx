756fb33eaf3e446c589a1c4e178e18027303ac27
Eddiebee <edbeeotudor@gmail.com>
2022-06-10 21:02:18 +0100

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 import Crew from "./pages/Crew";
 import Destination from "./pages/Destination";
-import Home from "./pages/Home";
 import Technology from "./pages/Technology";
 
 function App() {
-  return <Crew />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="destination" element={<Destination />} />
+        <Route path="technology" element={<Technology />} />
+        <Route path="crew" element={<Crew />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
