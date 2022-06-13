d1197781de7ece8a0695b299598ef6781028b072
Eddiebee <edbeeotudor@gmail.com>
2022-06-13 10:57:41 +0100

    Implement state for MenuBar

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,6 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import MenuBar from "./MenuBar";
 
 export type MobileNavProps = {
   children?: ReactNode;
@@ -8,13 +9,23 @@ export type MobileNavProps = {
 };
 
 const MobileNav = ({ children, logo }: MobileNavProps) => {
+  const [showMenuBar, setShowMenuBar] = useState(false);
   return (
-    <nav className="flex w-100 justify-between items-center ">
-      <Link to="/">
-        <img src={logo} alt="space tourism logo" />
-      </Link>
-      <AiOutlineMenu className="text-2xl text-secondary font-bold" />
-    </nav>
+    <>
+      <nav className="flex w-100 justify-between items-center ">
+        <Link to="/">
+          <img src={logo} alt="space tourism logo" />
+        </Link>
+        <AiOutlineMenu
+          className="text-2xl text-secondary font-bold cursor-pointer"
+          role="button"
+          onClick={() => {
+            setShowMenuBar(!showMenuBar);
+          }}
+        />
+      </nav>
+      {showMenuBar && <MenuBar />}
+    </>
   );
 };
 
