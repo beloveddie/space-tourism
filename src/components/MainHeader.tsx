b0284303ddb23dd8d7e11d49f8d2b758eaef77e8
Eddiebee <edbeeotudor@gmail.com>
2022-06-14 13:29:48 +0100

    Pass routes data from MainHeader

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,10 +1,17 @@
 import logo from "../assets/shared/logo.svg";
 import MobileNav from "./MobileNav";
 
+const routes = [
+  { title: "home", url: "/" },
+  { title: "destination", url: "/destination" },
+  { title: "crew", url: "/crew" },
+  { title: "technology", url: "/technology" },
+];
+
 export const MainHeader = () => {
   return (
     <header className="w-full bg-transparent fixed top-0 p-4">
-      <MobileNav logo={logo} />
+      <MobileNav logo={logo} routes={routes} />
     </header>
   );
 };
