d9ccfe893e34837fc4d2f55d2596da6591118d10
Eddiebee <edbeeotudor@gmail.com>
2022-06-14 15:14:07 +0100

    Configure Dynamic route for destination page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { Routes, Route } from "react-router-dom";
 import Crew from "./pages/Crew";
 import Destination from "./pages/Destination";
 import Technology from "./pages/Technology";
+import DestinationDetail from "./pages/DestinationDetail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="destination" element={<Destination />} />
+        <Route path="destination">
+          <Route index element={<Destination />} />
+          <Route path=":destination" element={<DestinationDetail />} />
+        </Route>
         <Route path="technology" element={<Technology />} />
         <Route path="crew" element={<Crew />} />
       </Route>
