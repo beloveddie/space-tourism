a9251547e8cdb4f9858af8d6c360be8a2b8acf2d
Eddiebee <edbeeotudor@gmail.com>
2022-06-15 17:34:41 +0100

    Add Destination Layout element

---
+++ src/components/Destination/DestinationLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const DestinationLayout = () => {
+  return (
+    <div>
+      DestinationLayout
+      <Outlet />
+    </div>
+  );
+};
+
+export default DestinationLayout;
