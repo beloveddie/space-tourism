87bde3e56d29888776a61f53f7fe96b68e04eb64
Eddiebee <edbeeotudor@gmail.com>
2022-06-15 19:23:30 +0100

    Dynamically pass data

--- src/components/Destination/DestinationArticle.tsx
+++ src/components/Destination/DestinationArticle.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-const DestinationArticle = () => {
+type TDestinationArticleProps = {
+  title: string;
+  description: string;
+  travel: string;
+  distance: string;
+};
+
+const DestinationArticle = ({
+  title,
+  description,
+  distance,
+  travel,
+}: TDestinationArticleProps) => {
   return (
     <article className="pt-12">
-      <h2 className="text-[56px]">EUROPA</h2>
+      <h2 className="text-[56px]">{title}</h2>
 
       <p className="text-secondary text-base w-11/12 mx-auto pb-10 border-b-2 border-selection">
-        See our planet as you’ve never seen it before. A perfect relaxing trip
-        away to help regain perspective and come back refreshed. While you’re
-        there, take in some history by visiting the Luna 2 and Apollo 11 landing
-        sites.
+        {description}
       </p>
 
       <footer className=" pt-5">
@@ -17,13 +26,13 @@ const DestinationArticle = () => {
           <p className="sub-heading-2 text-secondary leading-loose">
             AVG. DISTANCE
           </p>
-          <p className="sub-heading-1">384,400 KM</p>
+          <p className="sub-heading-1">{distance.toUpperCase()}</p>
         </div>
         <div>
           <p className="sub-heading-2 text-secondary leading-loose">
             EST. TRAVEL TIME
           </p>
-          <time className="sub-heading-1 pb-8">3 DAYS</time>
+          <time className="sub-heading-1 pb-8">{travel.toUpperCase()}</time>
         </div>
       </footer>
     </article>
--- src/components/Destination/DestinationImage.tsx
+++ src/components/Destination/DestinationImage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import moon from "../../assets/destination/image-moon.png";
 
-const DestinationImage = () => {
+export type TDestinationImages = { png: string; webp: string };
+
+export type TDestinationImageProps = {
+  images: TDestinationImages;
+};
+
+const DestinationImage = ({ images }: TDestinationImageProps) => {
   return (
     <div className="w-full flex justify-center mt-8">
-      <img src={moon} alt="moon" className="w-44 h-44 text-center" />
+      <picture className="w-44 h-44 text-center">
+        <source srcSet={images.webp} />
+        <img src={images.png} alt="moon" />
+      </picture>
     </div>
   );
 };
