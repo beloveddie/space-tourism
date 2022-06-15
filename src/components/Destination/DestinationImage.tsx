7cb7e7b751444f9901314654f023713a06ecadb7
Eddiebee <edbeeotudor@gmail.com>
2022-06-15 19:32:46 +0100

    Clean up

--- src/components/Destination/DestinationImage.tsx
+++ src/components/Destination/DestinationImage.tsx
@@ -8,7 +8,7 @@ export type TDestinationImageProps = {
 
 const DestinationImage = ({ images }: TDestinationImageProps) => {
   return (
-    <div className="w-full flex justify-center mt-8">
+    <div className="w-full flex justify-center mt-10">
       <picture className="w-44 h-44 text-center">
         <source srcSet={images.webp} />
         <img src={images.png} alt="moon" />
