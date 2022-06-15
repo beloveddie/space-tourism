62d495aba02c14ac8b0c72ec9be87531cb0ec218
Eddiebee <edbeeotudor@gmail.com>
2022-06-15 18:27:25 +0100

    Setup Layout for Destination page... cool features

--- src/components/Destination/DestinationLayout.tsx
+++ src/components/Destination/DestinationLayout.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import SideNumbers from "../ui/SideNumbers";
+import Title from "../ui/Title";
+import DestinationNav from "./DestinationNav";
+
+const destinationLinks = ["moon", "mars", "europa", "titan"];
 
 const DestinationLayout = () => {
   return (
-    <div>
-      DestinationLayout
-      <Outlet />
-    </div>
+    <section className="Destination min-h-screen w-full text-center ">
+      <div className="pt-28 flex justify-center gap-3">
+        <SideNumbers index="1" dark={true} />
+        <Title title="Pick your destination" />
+      </div>
+      <DestinationNav destinationLinks={destinationLinks} />
+      <section className="-mt-64">
+        <Outlet />
+      </section>
+    </section>
   );
 };
 
