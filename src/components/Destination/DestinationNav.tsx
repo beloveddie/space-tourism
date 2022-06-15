3a1e9f84d92ad099d6de03098bebab3a373d7028
Eddiebee <edbeeotudor@gmail.com>
2022-06-15 17:34:06 +0100

    Abstract Destination Navigation

---
+++ src/components/Destination/DestinationNav.tsx
@@ -0,0 +1,30 @@
+import { nanoid } from "nanoid";
+import React from "react";
+import { Link } from "react-router-dom";
+
+type TDestinationLinks = string[];
+
+type TDestinationNavProps = {
+  destinationLinks: TDestinationLinks;
+};
+
+const DestinationNav = ({ destinationLinks }: TDestinationNavProps) => {
+  return (
+    <ul className="flex mt-6 gap-7 justify-center">
+      {destinationLinks.map((destinationLink) => (
+        <li key={nanoid()}>
+          <h5 className="nav-text text-secondary">
+            <Link
+              to={`/destination/${destinationLink}`}
+              className="pb-1 hover:border-b-4 hover:border-primary/20 active:border-primary focus:text-primary focus:border-b-4 focus:border-primary"
+            >
+              {destinationLink.toUpperCase()}
+            </Link>
+          </h5>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default DestinationNav;
