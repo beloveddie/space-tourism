a98ee1366f3845927671d2daec6512ab230a4cd9
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 13:02:45 +0100

    Fine tune image for crew to meet ui

--- src/components/Crew/CrewDetail.tsx
+++ src/components/Crew/CrewDetail.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CrewImage from "./CrewImage";
 
 const CrewDetail = () => {
-  return <div>CrewDetail</div>;
+  return (
+    <>
+      <div className="w-[90%] h-auto mx-auto border-b-[0.14rem] border-selection">
+        <CrewImage />
+      </div>
+    </>
+  );
 };
 
 export default CrewDetail;
---
+++ src/components/Crew/CrewImage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PageImageCnt from "../ui/PageImageCnt";
+
+const CrewImage = () => {
+  return (
+    <PageImageCnt>
+      <picture className="w-[226.15px]">
+        <source />
+        <img
+          src="https://res.cloudinary.com/enthusiasm/image/upload/v1655375345/space-tourism/crew/image-anousheh-ansari_kupxst.webp"
+          alt=""
+        />
+      </picture>
+    </PageImageCnt>
+  );
+};
+
+export default CrewImage;
