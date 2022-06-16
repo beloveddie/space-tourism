b54527c4b40e37641c5b278e91c40267c4967227
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 14:01:57 +0100

    Programmatically display Crew Page article

--- src/components/Crew/CrewArticle.tsx
+++ src/components/Crew/CrewArticle.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const CrewArticle = () => {
+type TCrewArticleProps = {
+  name: string;
+  role: string;
+  bio: string;
+};
+
+const CrewArticle = ({ name, role, bio }: TCrewArticleProps) => {
   return (
     <article className="pt-4">
-      <h5 className="text-base text-primary/50">FLIGHT ENGINEER</h5>
-      <p className="sub-heading-1">ANOUSHEH ANSARI</p>
+      <h5 className="text-base text-primary/50">{role.toUpperCase()}</h5>
+      <p className="sub-heading-1">{name.toUpperCase()}</p>
       <p className="text-secondary pt-4 text-base w-[90%] mx-auto pb-4">
-        Anousheh Ansari is an Iranian American engineer and co-founder of Prodea
-        Systems. Ansari was the fourth self-funded space tourist, the first
-        self-funded woman to fly to the ISS, and the first Iranian in space.
+        {bio}
       </p>
     </article>
   );
