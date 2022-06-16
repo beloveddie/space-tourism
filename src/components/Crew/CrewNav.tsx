326cd0766749f068956eccdcd7c2c395e09a9c8b
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 13:56:41 +0100

    Progammatically create navigation

--- src/components/Crew/CrewNav.tsx
+++ src/components/Crew/CrewNav.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { BsCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { TPageLinks } from "../../shared/types";
 
-const CrewNav = () => {
+type TCrewNavProps = {
+  crewNavLinks: TPageLinks;
+};
+const CrewNav = ({ crewNavLinks }: TCrewNavProps) => {
   return (
     <ul className="flex mt-8 gap-7 justify-center mx-auto w-full">
-      <Link
-        to="#"
-        className="text-primary/20 hover:text-primary/50 focus:text-primary text-[12px]"
-      >
-        <BsCircleFill />
-      </Link>
+      {crewNavLinks.map((crewNavLink) => (
+        <Link
+          to={`/crew/${crewNavLink}`}
+          className="text-primary/20 hover:text-primary/50 focus:text-primary text-[12px]"
+        >
+          <BsCircleFill />
+        </Link>
+      ))}
     </ul>
   );
 };
