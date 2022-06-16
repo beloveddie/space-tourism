6dc150848f2cf79631e7ec953e570ae980be4329
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 11:13:52 +0100

    Add text type for alt image element attribute

--- src/components/Destination/DestinationImage.tsx
+++ src/components/Destination/DestinationImage.tsx
@@ -4,14 +4,15 @@ export type TDestinationImages = { png: string; webp: string };
 
 export type TDestinationImageProps = {
   images: TDestinationImages;
+  text: string;
 };
 
-const DestinationImage = ({ images }: TDestinationImageProps) => {
+const DestinationImage = ({ images, text }: TDestinationImageProps) => {
   return (
     <div className="w-full flex justify-center mt-10">
       <picture className="w-44 h-44 text-center">
         <source srcSet={images.webp} />
-        <img src={images.png} alt="moon" />
+        <img src={images.png} alt={text} />
       </picture>
     </div>
   );
