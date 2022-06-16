e74247c18c64a9a9cfedb58fbe859f04aea7b375
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 13:28:09 +0100

    Add crew Article component

---
+++ src/components/Crew/CrewArticle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CrewArticle = () => {
+  return (
+    <article className="pt-4">
+      <h5 className="text-base text-primary/50">FLIGHT ENGINEER</h5>
+      <p className="sub-heading-1">ANOUSHEH ANSARI</p>
+      <p className="text-secondary pt-4 text-base w-[90%] mx-auto pb-4">
+        Anousheh Ansari is an Iranian American engineer and co-founder of Prodea
+        Systems. Ansari was the fourth self-funded space tourist, the first
+        self-funded woman to fly to the ISS, and the first Iranian in space.
+      </p>
+    </article>
+  );
+};
+
+export default CrewArticle;
