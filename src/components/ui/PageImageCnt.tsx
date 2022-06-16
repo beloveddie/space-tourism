57704bed3d6e4a7d2a09a38eaf50ef9347453839
Eddiebee <edbeeotudor@gmail.com>
2022-06-16 12:10:53 +0100

    Abstract Page Image Container

---
+++ src/components/ui/PageImageCnt.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageImageCnt = ({ children }: React.PropsWithChildren) => {
+  return <div className="w-full flex justify-center mt-10">{children}</div>;
+};
+
+export default PageImageCnt;
