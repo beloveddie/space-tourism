6a35fa5b155dce33d3535474d4c175b1a2f0fe48
Eddiebee <edbeeotudor@gmail.com>
2022-06-20 13:27:28 +0100

    Abstract function to find the given data(per page)

--- src/utils/findData.ts
+++ src/utils/findData.ts
@@ -1,4 +1,12 @@
-export const findData = (originData: [], searchItem: string) => {
-  // const data = originData.find(item  => item.name === searchItem)
-  return " ";
+import { TCrew } from "../components/Crew/CrewDetail";
+import { TDestination } from "../components/Destination/DestinationDetail";
+import { TTechnology } from "../components/Technology/TechnologyDetail";
+import { reslugify } from "./reslugify";
+
+type TExpectedData = TDestination[] & TCrew[] & TTechnology[];
+
+export const findData = (data: TExpectedData, searchString: string) => {
+  return data.find(
+    (item) => item.name.toLowerCase() === reslugify(searchString.toLowerCase())
+  );
 };
