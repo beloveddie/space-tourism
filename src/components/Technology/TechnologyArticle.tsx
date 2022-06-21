3733c6c7da2b516cf67461d8686246e0c3bbec75
Eddiebee <edbeeotudor@gmail.com>
2022-06-21 13:19:57 +0100

    A lil clean up

--- src/components/Technology/TechnologyArticle.tsx
+++ src/components/Technology/TechnologyArticle.tsx
@@ -15,7 +15,7 @@ const TechnologyArticle = ({
     <article className="text-center mt-32 mb-8">
       <h5 className="text-[12px] text-secondary">{tag.toUpperCase()}...</h5>
       <h4 className="">{name.toUpperCase()}</h4>
-      <p className="mt-3 mb-3 text-secondary w-11/12 leading-8 mx-auto">
+      <p className="mt-3 mb-3 text-base text-secondary w-11/12 leading-8 mx-auto">
         {description}
       </p>
     </article>
