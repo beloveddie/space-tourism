c1b22af6709e678a27d8f8388cf6b5e49b202959
Eddiebee <edbeeotudor@gmail.com>
2022-06-21 10:48:36 +0100

    Setup TechnologyDetail component

--- src/components/Technology/TechnologyDetail.tsx
+++ src/components/Technology/TechnologyDetail.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import DataContext from "../../context/data";
+import { TDataContext } from "../../shared/types";
+import { findData } from "./../../utils/findData";
+import TechnologyArticle from "./TechnologyArticle";
+import TechnologyImage from "./TechnologyImage";
+
+export type TTechnologyImage = { portrait: string; landscape: string };
+
+export type TTechnology = {
+  name: string;
+  images: TTechnologyImage;
+  description: string;
+};
 
 const TechnologyDetail = () => {
-  return <div>TechnologyDetail</div>;
+  const { technology } = useContext(DataContext) as TDataContext;
+  const params = useParams() as { technology: string };
+
+  const technologyFromUrl = findData(
+    technology,
+    params.technology
+  ) as TTechnology;
+
+  return (
+    <>
+      <TechnologyImage
+        text={technologyFromUrl!.name}
+        images={technologyFromUrl.images}
+      />
+      <TechnologyArticle
+        name={technologyFromUrl!.name}
+        tag={"The terminology"}
+        description={technologyFromUrl!.description}
+      />
+    </>
+  );
 };
 
 export default TechnologyDetail;
