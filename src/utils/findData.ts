b154504403b355913dc770aef5908d9bfb10ecb3
Eddiebee <edbeeotudor@gmail.com>
2022-06-21 10:50:48 +0100

    A lil clean up

--- src/utils/findData.ts
+++ src/utils/findData.ts
@@ -1,9 +1,4 @@
-import { TCrew } from "../components/Crew/CrewDetail";
-import { TDestination } from "../components/Destination/DestinationDetail";
-import { TTechnology } from "../components/Technology/TechnologyDetail";
 import { reslugify } from "./reslugify";
-
-type TExpectedData = TDestination[] & TCrew[] & TTechnology[];
 
 export const findData = (data: [], searchString: string): unknown => {
   return data.find(
