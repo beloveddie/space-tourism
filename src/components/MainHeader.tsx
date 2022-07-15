687ba86b5263eebb0c4f4a7d80208dd2d810d2e2
Eddiebee <edbeeotudor@gmail.com>
2022-07-15 13:55:35 +0100

    Clean MainHeader component

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,23 +1,17 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/shared/logo.svg";
+import { routes } from "../shared/routes";
 import MobileNav from "./MobileNav";
 import TabletNav from "./TabletNav";
 
-const routes = [
-  { title: "home", url: "/" },
-  { title: "destination", url: "/destination" },
-  { title: "crew", url: "/crew" },
-  { title: "technology", url: "/technology" },
-];
-
 export const MainHeader = () => {
   return (
-    <header className="w-full bg-transparent fixed top-0 p-4 flex w-100 justify-between items-center">
+    <header className="w-full bg-transparent fixed top-0 p-4 flex w-100 justify-between items-center md:p-8">
       <Link to="/">
-        <img src={logo} alt="space tourism logo" />
+        <img src={logo} alt="space tourism logo" className="md:w-12" />
       </Link>
       <MobileNav routes={routes} />
-      <TabletNav />
+      <TabletNav routes={routes} />
     </header>
   );
 };
