a580c04a82bd6c2fba08bd798b1b03d8a178c4aa
Eddiebee <edbeeotudor@gmail.com>
2022-07-15 13:57:01 +0100

    Build up TabletNav component

--- src/components/TabletNav/TabletNav.tsx
+++ src/components/TabletNav/TabletNav.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavProps } from "../../shared/types";
+import TabletNavList from "./TabletNavList";
 
-const TabletNav = () => {
-  return <nav className="hidden md:flex lg:hidden">TabletNav</nav>;
+const TabletNav = ({ routes }: NavProps) => {
+  const params = useParams();
+  const [activeNavLink, setActiveNavLink] = useState("");
+
+  useEffect(() => {
+    if (!Object.keys(params).toString()) {
+      setActiveNavLink("home");
+    } else {
+      setActiveNavLink(Object.keys(params).toString());
+    }
+  }, [params]);
+
+  return (
+    <nav className="hidden md:flex lg:hidden absolute bg-primary/5 w-100 h-24 top-0 right-0 text-center items-center p-14">
+      <TabletNavList items={routes} activeNavLink={activeNavLink} />
+    </nav>
+  );
 };
 
 export default TabletNav;
--- src/components/TabletNav/TabletNavList.tsx
+++ src/components/TabletNav/TabletNavList.tsx
@@ -1,7 +1,25 @@
+import { nanoid } from "nanoid";
 import React from "react";
+import { TItems } from "../../shared/types";
+import TabletNavItem from "./TabletNavItem";
 
-const TabletNavList = () => {
-  return <div>TabletNavList</div>;
+type TTabletItemsProps = {
+  items: TItems;
+  activeNavLink: string;
+};
+
+const TabletNavList = ({ items, activeNavLink }: TTabletItemsProps) => {
+  return (
+    <ul className="flex gap-10">
+      {items.map((item, index) => (
+        <TabletNavItem
+          key={nanoid()}
+          item={item}
+          activeNavLink={activeNavLink}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default TabletNavList;
