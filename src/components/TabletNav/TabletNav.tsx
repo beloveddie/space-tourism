7f11dde62f20b0f19b04c23afb8fb7fc8385473b
Eddiebee <edbeeotudor@gmail.com>
2022-07-15 11:27:43 +0100

    Initial setup of TabletNav components

---
+++ src/components/TabletNav/TabletNav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TabletNav = () => {
+  return <nav className="hidden md:flex lg:hidden">TabletNav</nav>;
+};
+
+export default TabletNav;
---
+++ src/components/TabletNav/TabletNavList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TabletNavList = () => {
+  return <div>TabletNavList</div>;
+};
+
+export default TabletNavList;
