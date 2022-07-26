3f4a6bf82ef0b40a5eff3a1e592a65e8fa0834b9
Eddiebee <edbeeotudor@gmail.com>
2022-07-26 17:31:27 +0100

    Make Technology Page image dynamic... wow!

--- src/components/Technology/TechnologyImage.tsx
+++ src/components/Technology/TechnologyImage.tsx
@@ -6,11 +6,12 @@ export type TTechnologyProps = {
   images: TTechnologyImage;
   text: string;
 };
+
 const TechnologyImage = ({ images, text }: TTechnologyProps) => {
   return (
     <picture>
       <img
-        src={images.portrait}
+        src={window.innerWidth > 1024 ? images.portrait : images.landscape}
         alt={text}
         className="mt-6 w-full h-100 md:mt-16 lg:mt-0"
       />
